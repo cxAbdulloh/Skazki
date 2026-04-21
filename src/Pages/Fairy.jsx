d8a39ab1180da8444b3177603be920dc95c0cf1a
unknown <you@example.com>
2026-04-21 16:39:10 +0500

    add buttons

--- src/Pages/Fairy.jsx
+++ src/Pages/Fairy.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { useLanguage } from "../LanguageContext";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import "./Fairy.css";
-import { assets } from "../assets/assets";
 import Footer from "../Components/Footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faTelegram } from "@fortawesome/free-brands-svg-icons";
@@ -110,7 +108,7 @@ const Fairy = () => {
           </div>
 
           <div className={`nav-right-group ${isMenuOpen ? "open" : ""}`}>
-          <div id="social-links-mobile">
+            <div id="social-links-mobile">
               <a
                 href="https://t.me/fotoskaz_bot"
                 target="_blank"
@@ -184,10 +182,31 @@ const Fairy = () => {
               ))}
             </div>
           </div>
-          <button className="accordion-button"><a href="https://t.me/fotoskaz_bot">{t("delevery")}</a></button>
+          <div className="bottom-button-containers">
+            <button className="accordion-button">
+              <a href="https://t.me/fotoskaz_bot">{t("delevery")}</a>
+            </button>
+            <button className="second-accordion-button">
+              <a href="tel:+998919767600" className="phone">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l2.27-2.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+                <span>+998919767600</span>
+              </a>
+            </button>
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
