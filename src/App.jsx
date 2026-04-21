b5da78900836f50ea4c0f8ddffb59d1550f06d03
unknown <you@example.com>
2026-04-21 16:58:47 +0500

    add the stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion"; // Exit animatsiyasi uchun shart
+import { AnimatePresence } from "framer-motion";
 import { LanguageProvider } from "./LanguageContext";
 import Home from "./Pages/Home";
 import Collection from "./Pages/Collection";
-import Loader from "./Loader"; // Sizning Loader komponentingiz
+import Loader from "./Loader";
 import Fairy from "./Pages/Fairy";
-
+import "./App.css"; 
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Loader ichidagi sanash 3 soniya davom etadi (100 * 30ms)
-    // Shuning uchun App.js 3-3.2 soniya kutishi kerak
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 3200); 
+    }, 3200);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <LanguageProvider>
-      {/* AnimatePresence loaderning silliq g'oyib bo'lishini ta'minlaydi */}
       <AnimatePresence mode="wait">
         {isLoading ? (
           <Loader key="loader" />
         ) : (
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/memory" element={<Collection/>} />
-              <Route path="/fairy" element={<Fairy/>} />
-            </Routes>
-          </Router>
+          <div className="app-main-wrapper">
+            {/* Sehrli yulduzchalar foni */}
+            <div className="magic-stars-overlay">
+              {[...Array(40)].map((_, i) => (
+                <div key={i} className="magic-star"></div>
+              ))}
+            </div>
+
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/memory" element={<Collection />} />
+                <Route path="/fairy" element={<Fairy />} />
+              </Routes>
+            </Router>
+          </div>
         )}
       </AnimatePresence>
     </LanguageProvider>
